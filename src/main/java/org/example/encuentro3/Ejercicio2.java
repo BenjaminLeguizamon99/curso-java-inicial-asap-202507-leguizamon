9b93447cefdd9f32b7f02f565cbf8436ca972aee
Benjamin Leguizamon <bleguizamon@asap-consulting.net>
2025-07-25 10:36:42 -0300

    Ejercicio 2 correspondiente al encuentro 3

---
+++ src/main/java/org/example/encuentro3/Ejercicio2.java
@@ -0,0 +1,44 @@
+package org.example.encuentro3;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Ejercicio2 {
+    public static void main(String[] args) {
+        // Escribir un programa que pida 6 numeros por teclado, los almacene en un array y que luego muestre el maximo
+        // valor, el minimo y las posiciones que ocupan en el array
+        Scanner scanner = new Scanner(System.in);
+
+        int numero = 1;
+        ArrayList<Integer> numeros = new ArrayList<>();
+        while (numero <= 6) {
+            numero += 1;
+            System.out.println("Ingrese un numero: ");
+            numeros.add(scanner.nextInt());
+        }
+        System.out.println("Los numeros ingresados son: " + numeros);
+
+        // Calculo del minimo y su posicion en el array
+        int menorNumero = numeros.get(0);
+        int posicionMenorNumero = 0;
+
+        for(int i = 0; i < numeros.size(); i++) {
+            if(numeros.get(i) < menorNumero) {
+                menorNumero = numeros.get(i);
+                posicionMenorNumero = i + 1;
+            }
+        }
+
+        System.out.println("El menor numero es: " + menorNumero + " y ocupa la posicion: " + posicionMenorNumero);
+
+        int mayorNumero = numeros.get(0);
+        int posicionMayorNumero = 0;
+
+        for(int i = 0; i < numeros.size(); i++) {
+            if(numeros.get(i) > mayorNumero){
+                mayorNumero = numeros.get(i);
+                posicionMayorNumero = i + 1;
+            }
+        }
+        System.out.println("El mayor numero es: " + mayorNumero + " y ocupa la posicion: " + posicionMayorNumero);
+    }
+}
