1c61b8123534722f73592a6f4b57c4f27746331b
Benjamin Leguizamon <bleguizamon@asap-consulting.net>
2025-07-29 17:14:19 -0300

    Ejercicio 2 correspondiente al encuentro 3 sin usar arraylist

--- src/main/java/org/example/encuentro3/Ejercicio2.java
+++ src/main/java/org/example/encuentro3/Ejercicio2.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class Ejercicio2 {
     public static void main(String[] args) {
-        // Escribir un programa que pida 6 numeros por teclado, los almacene en un array y que luego muestre el maximo
+/*        // Escribir un programa que pida 6 numeros por teclado, los almacene en un array y que luego muestre el maximo
         // valor, el minimo y las posiciones que ocupan en el array
         Scanner scanner = new Scanner(System.in);
 
@@ -39,6 +39,41 @@ public class Ejercicio2 {
                 posicionMayorNumero = i + 1;
             }
         }
-        System.out.println("El mayor numero es: " + mayorNumero + " y ocupa la posicion: " + posicionMayorNumero);
+        System.out.println("El mayor numero es: " + mayorNumero + " y ocupa la posicion: " + posicionMayorNumero);*/
+
+
+        // Mismo ejercicio sin usar arraylist
+        Scanner scanner = new Scanner(System.in);
+        int[] numeros = new int[6];
+
+        // Cargamos los numeros al array
+        for(int i = 0; i < numeros.length; i++) {
+            System.out.println("Ingrese un numero: ");
+            numeros[i] = scanner.nextInt();
+        }
+
+        // Buscamos el maximo valor
+        int maximoValor = numeros[0];
+        int minimoValor = numeros[0];
+        int posMaxValor = 0;
+        int posMinValor = 0;
+
+        for(int i = 0; i < numeros.length; i++) {
+            if(numeros[i] > maximoValor) {
+                maximoValor = numeros[i];
+                posMaxValor = i + 1;
+            }
+        }
+
+        System.out.println("El maximo valor es de: " + maximoValor + " y se encuentra en la posicion: " + posMaxValor);
+
+        for(int i = 0; i < numeros.length; i++) {
+            if(numeros[i] < minimoValor) {
+                minimoValor = numeros[i];
+                posMinValor = i + 1;
+            }
+        }
+
+        System.out.println("El maximo valor es de: " + minimoValor + " y se encuentra en la posicion: " + posMinValor);
     }
 }
