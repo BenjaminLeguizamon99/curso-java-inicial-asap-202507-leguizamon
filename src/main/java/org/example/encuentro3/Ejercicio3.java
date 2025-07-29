77d268423cb3ef18fd2ae05dbf91923ab9eff4e9
Benjamin Leguizamon <bleguizamon@asap-consulting.net>
2025-07-29 17:44:13 -0300

    Ejercicio 3 del encuentro 3 pero sin usar arraylist

--- src/main/java/org/example/encuentro3/Ejercicio3.java
+++ src/main/java/org/example/encuentro3/Ejercicio3.java
@@ -1,11 +1,12 @@
 package org.example.encuentro3;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Ejercicio3 {
     public static void main(String[] args){
-        Scanner scanner = new Scanner(System.in);
+/*        Scanner scanner = new Scanner(System.in);
         ArrayList<Integer> numeros = new ArrayList<>();
 
         // se ingresan los numeros y se cargan al array
@@ -36,6 +37,37 @@ public class Ejercicio3 {
         }
         if(esCapicua){
             System.out.println("El array SI es capicua");
+        }*/
+
+
+        // Mismo ejercicio usando array en lugar de arraylist
+        Scanner scanner = new Scanner(System.in);
+        int[] numeros = new int[5];
+        int[] numerosAlReves = new int[5];
+        for(int i = 0; i < numeros.length; i++) {
+            System.out.println("Ingrese un numero entero: ");
+            int num = scanner.nextInt();
+            numeros[i] = num;
+        }
+
+        int j = 0;
+        for(int i = numeros.length - 1; i >= 0; i--) {
+            numerosAlReves[j] = numeros[i];
+            j++;
+        }
+
+        // Verificamos si es capicua
+        boolean esCapicua = true;
+        for(int i = 0; i < numeros.length; i++) {
+            if(numeros[i] != numerosAlReves[i]) {
+                esCapicua = false;
+            }
+        }
+
+        if(esCapicua) {
+            System.out.println("El array es capicua");
+        } else {
+            System.out.println("El array no es capicua");
         }
     }
 
